15dd4b324404d32f6546241019340aacba0f5f4e
Sérgio Toledo <contato@sergiotoledo.com.br>
2019-07-31 23:47:21 -0300

    Change seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,36 +2,34 @@
 
 joao = Person.create(first_name: 'João', last_name: 'da Silva Santos', gender: 'male', birthdate: '1977-09-13')
 maria = Person.create(first_name: 'Maria', last_name: 'da Silva Santos', gender: 'female', birthdate: '1978-11-06')
-ana = Person.create(first_name: 'Ana', last_name: 'da Silva Santos', gender: 'female', birthdate: '2013-06-17')
+rosa = Person.create(first_name: 'Rosa', last_name: 'da Silva Santos', gender: 'female', birthdate: '2013-06-17')
 pedro = Person.create(first_name: 'Pedro', last_name: 'da Silva Santos', gender: 'male', birthdate: '2015-12-16')
+jose = Person.create(first_name: 'José', last_name: 'da Silva Santos', gender: 'male', birthdate: '1950-01-01')
+ana = Person.create(first_name: 'Ana', last_name: 'da Silva Santos', gender: 'male', birthdate: '2052-10-10')
 
-conjuge = Kinship.create(name: 'Conjuge')
-filho = Kinship.create(name: 'Filho')
-filha = Kinship.create(name: 'Filha')
+spouse = Kinship.create(name: 'spouse')
+son = Kinship.create(name: 'son')
+daughter = Kinship.create(name: 'daughter')
+father = Kinship.create(name: 'father')
+mother = Kinship.create(name: 'mother')
 
 Kinship.create(
   [
-    { name: 'Pai' },
-    { name: 'Mãe' },
-    { name: 'Irmão' },
-    { name: 'Irmã' },
-    { name: 'Tio' },
-    { name: 'Tia' },
-    { name: 'Sobrinho' },
-    { name: 'Sobrinha' },
-    { name: 'Primo' },
-    { name: 'Prima' },
-    { name: 'Avô' },
-    { name: 'Avó' },
-    { name: 'Neto' },
-    { name: 'Neta' },
-    { name: 'Bisavô' },
-    { name: 'Bisavó' },
-    { name: 'Bisneto' },
-    { name: 'Bisneta' }
+    { name: 'brother' },
+    { name: 'sister' },
+    { name: 'uncle' },
+    { name: 'aunt' },
+    { name: 'nephew' },
+    { name: 'niece' },
+    { name: 'grandfather' },
+    { name: 'grandmother' },
+    { name: 'grandson' },
+    { name: 'granddaughter' }
   ]
 )
 
-Relationship.create(person: joao, relative_person: maria, kinship: conjuge)
-Relationship.create(person: joao, relative_person: ana, kinship: filha)
-Relationship.create(person: joao, relative_person: pedro, kinship: filho)
+Relationship.create(person: joao, relative_person: maria, kinship: spouse)
+Relationship.create(person: joao, relative_person: rosa, kinship: daughter)
+Relationship.create(person: joao, relative_person: pedro, kinship: son)
+Relationship.create(person: joao, relative_person: jose, kinship: father)
+Relationship.create(person: joao, relative_person: ana, kinship: mother)
